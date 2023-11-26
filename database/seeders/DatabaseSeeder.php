ff918472141eaec09b5a7fd7567479e194e85ed0
Avakyan Sasun <savakyan@lab4u.ru>
2023-11-26 14:09:38 +0300

    Installed Laravel Livewire

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+//         \App\Models\User::factory()->create([
+//             'name' => 'Admin',
+//             'email' => 'admin@admin.com',
+//             'password' => '$2y$12$7Sgzsk5XDhpb1./6APWF5Old2LNmEqUe.qEd8hCXIz5o2e3Qc5G12',
+//         ]);
     }
 }
